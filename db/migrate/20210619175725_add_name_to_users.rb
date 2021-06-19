3a80b193439302de269bbd01d216506e52071743
“Sheryyy1019” <sakaimizuki1019@gmail.com>
2021-06-20 03:01:30 +0900

    Usersテーブルにnameカラム等追加できました

--- db/migrate/20210619175725_add_name_to_users.rb
+++ db/migrate/20210619175725_add_name_to_users.rb
@@ -1,4 +1,4 @@
-class AddDetailsToUsers < ActiveRecord::Migration[6.0]
+class AddNameToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :name, :string
     add_column :users, :profile, :text
